5aba3a135fae14139612e15a51b0f83ad39be49e
BrayanCaro <rojosuper@ciencias.unam.mx>
2019-10-24 09:55:16 -0500

    El test ya esta listo Solo realiza: ./test_runner # Donde # es un entero, si no lo ponemos solo se corre una prueba en automatico Y # es la cantidad de veces que las pruebas se van a repetir

--- test_runner.sh
+++ test_runner.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-meson build
+if ! [ -d "build/" ]; then
+  meson build
+fi
 cd build
-meson test --repeat=10
 
-var=`cat meson-logs/testlog.json | grep -o "returncode\"\: -\?[06]" | grep -o "\-\?[06]"`
+if [ "$1" == "" ]; then
+  meson test --repeat=1
+else
+  meson test --repeat=$1
+fi
+
+total_test=4
+tests_fail_and_pass=`cat meson-logs/testlog.json | grep -o "returncode\"\: -\?[06]" | grep -o "\-\?[06]"`
+time_per_test=`cat meson-logs/testlog.json | grep -o "duration\"\: 0.\{5\}" | grep -o "0.\{5\}"`
 
 test_passed=0
 test_failed=0
-for addr in $var
+for test in $tests_fail_and_pass
 do
-  if [ $addr == "0" ]; then
+  if [ $test == "0" ]; then
     ((test_passed++))
   else
     ((test_failed++))
@@ -18,3 +27,10 @@ do
 done
 echo "Pasaron:" $test_passed " pruebas"
 echo "Fallarón:" $test_failed " pruebas"
+
+averange=0
+for test in $time_per_test
+do
+  averange=$(bc -l <<<"${test}+${averange}")
+done
+echo "El promedio de tiempo por prueba fue " $(bc -l <<<"${averange}/${total_test}") " seg"
